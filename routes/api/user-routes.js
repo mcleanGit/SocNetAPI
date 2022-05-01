b3f5d6c8587a3ca1991adc64046556785868736c
Don McLean <mclean@utoronto.ca>
2022-05-01 13:53:41 -0400

    initial draft api user-routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const {
+ getAllUser,
+ getUserById,
+ createUser,
+ updateUser, 
+ deleteUser
+} = require('../../controllers/user-controller');
+
+//api/users
+router
+ .route('/')
+ .get(getAllUser)
+ .post(createUser);
+
+ //api/users
+ router
+  .route('/:id')
+  .get(getUserById)
+  .post(updateUser)
+  .delete(deleteUser);
+
+  module.exports = router;
