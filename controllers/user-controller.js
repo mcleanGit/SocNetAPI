7138f1ef389d1c5968bc3fbf72d66bc141b5f363
Don McLean <mclean@utoronto.ca>
2022-05-01 14:05:31 -0400

    add getSingleUser

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,6 @@
+const res = require('express/lib/response');
 const { User, Thought } = require('../models');
+
 const userController = {
  // get all users
  getUsers(req, res) {
@@ -13,7 +15,22 @@ const userController = {
    });
  },
  // get single user by id
-
+ getSingleUser(req, res) {
+   User.findOne({_id: req.params.userId })
+    .select('-__v')
+    .populate('friends')
+    .populate('thoughts')
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        return res.status(404).json({ message: 'No user with this id!' });
+      }
+      res.json(dbUserData);
+    })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+ },
  // create new user
 
  // update user by id
