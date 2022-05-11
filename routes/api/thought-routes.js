169560f04b8fdfcb3694150beefd27711f24e56b
Don McLean <mclean@utoronto.ca>
2022-05-11 13:06:34 -0400

    add getThoughts getSingleThought to review

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,11 +1,25 @@
 const router = require('express').Router();
 
 const {
+ getThoughts,
+ getSingleThought,
  addThought,
  addReaction,
  removeThought,
  removeReaction
 } = require('../../controllers/thought-controller');
+
+// api/thoughts
+router.route.get(getThoughts); 
+
+// api/thoughts/<thoughtId>
+router.route('/:thoughtId').get(getSingleThought);
+
+// api/thoughts/<userId>
+router.route('/:userId').get(getThoughts);
+
+//api/thoughts/<userId>/<thoughtId>
+router.route('/:userId/thoughtId').get(getSingleThought);
 
 // /api/thought/<userId>
 router.route('/:userId').post(addThought);
