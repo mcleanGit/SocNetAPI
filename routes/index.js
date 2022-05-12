b7ddaea3a0f189466bfb6896547856a27eff3819
Don McLean <mclean@utoronto.ca>
2022-05-12 10:34:24 -0400

    req to _req

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-const router = require('express').Router;
+const router = require('express').Router();
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
-router.use((req, res) => {
+router.use((_req, res) => {
  return res.send('Wrong route!');
 });
 
