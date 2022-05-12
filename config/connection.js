bd132ba2088676730502a6f85a931b034576351a
Don McLean <mclean@utoronto.ca>
2022-05-12 13:44:07 -0400

    added mongoose.set debug log

--- config/connection.js
+++ config/connection.js
@@ -4,5 +4,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/SocialNet
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+// to log mongo queries being executed
+mongoose.set('debug', true);
 
 module.exports = mongoose.connection;
