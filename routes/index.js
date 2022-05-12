5d12c68ad594327de7185a427bcf8e2cc3af8ec6
Don McLean <mclean@utoronto.ca>
2022-05-12 13:44:52 -0400

    commented out Wrong route message

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
-router.use((_req, res) => {
- return res.send('Wrong route!');
-});
+//router.use((_req, res) => {
+ //return res.send('Wrong route!');
+//});
 
 module.exports = router;
