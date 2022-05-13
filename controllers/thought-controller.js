18c615b141802ab398614e07bebec5a2619ae539
Don McLean <mclean@utoronto.ca>
2022-05-13 17:04:35 -0400

    continued work on thoughts array and reactions

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -76,7 +76,6 @@ deleteThought(req, res) {
     });
   },
  // add reaction to thought
- // { params, body } changed back to req, res
   addReaction(req, res) {
    Thought.findOneAndUpdate(
     { _id: req.params.thoughtId },
@@ -91,29 +90,6 @@ deleteThought(req, res) {
    })
     .catch(err => res.json(err));
   },
-  /* remove thought -- see delete thought above
-  removeThought({ params }, res) {
-   Thought.findOneAndDelete({ _id: params.commentId })
-    .then(deletedThought => {
-     if (!deletedThought) {
-      return res.status(404).json({ message: 'No thought with this id!' });
-     }
-     return User.findOneAndUpdate(
-      { _id: params.userId },
-      { $pull: { thoughts: params.thoughtId } },
-      { new: true }
-     );
-    })
-    .then(dbUserData => {
-     if (!dbUserData) {
-      res.status(404).json({ message: 'No user found with this id!' });
-      return;
-     }
-     res.json(dbUserData);
-    })
-    .catch(err => res.json(err));
-  },
-  */
   // remove reaction
   removeReaction( { params }, res) {
    Thought.findOneAndUpdate(
