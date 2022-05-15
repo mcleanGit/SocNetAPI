c46ade69b2926c52ba88c35a33d28027f2bc4d3d
Don McLean <mclean@utoronto.ca>
2022-05-15 14:29:00 -0400

    check format of userName

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose');
 
 const UserSchema = new Schema(
   {
-    username: {
+    userName: {
       type: String,
       unique: true,
       required: true,
